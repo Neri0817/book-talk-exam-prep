28942b024ef66f2f979e326f0ee5c981b0c5b3db
Neri <91215462+Neri0817@users.noreply.github.com>
2023-02-16 23:39:53 +0200

    Update edit and delete functionality

--- cotrollers/booksController.js
+++ cotrollers/booksController.js
@@ -4,10 +4,10 @@ const { isAuth } = require("../middlewares/authMiddleware");
 const bookService = require("../services/bookService");
 const { getErrorMessage } = require("../utils/errorUtils");
 
+const Books = require("../models/Books");
+
 router.get("/catalog", async (req, res) => {
   const books = await bookService.getAll().lean();
-
-  console.log(books);
 
   res.render("books/catalog", { books });
 });
@@ -24,6 +24,38 @@ router.post("/create", isAuth, async (req, res) => {
       .status(400)
       .render("books/create", { error: getErrorMessage(error) });
   }
+
+  res.redirect("/books/catalog");
+});
+
+router.get("/:bookId/details", async (req, res) => {
+  const book = await bookService.getOne(req.params.bookId);
+  const isOwner = book.owner.toString() === req.user?._id;
+  const isWished = book.wishlist?.some((id) => id == req.user?._id);
+
+  res.render("books/details", { book, isOwner, isWished });
+});
+
+router.get("/:bookId/wish", isAuth, async (req, res) => {
+  await bookService.wish(req.user._id, req.params.bookId);
+
+  res.redirect(`/books/${req.params.bookId}/details`);
+});
+
+router.get("/:bookId/edit", isAuth, async (req, res) => {
+  const book = await bookService.getOne(req.params.bookId);
+
+  res.render("books/edit", { book });
+});
+
+router.post("/:bookId/edit", isAuth, async (req, res) => {
+  await Books.findByIdAndUpdate(req.params.bookId, req.body);
+
+  res.redirect(`/books/${req.params.bookId}/details`);
+});
+
+router.get("/:bookId/delete", isAuth, async (req, res) => {
+  await Books.findByIdAndDelete(req.params.bookId);
 
   res.redirect("/books/catalog");
 });
--- services/bookService.js
+++ services/bookService.js
@@ -2,8 +2,16 @@ const Books = require("../models/Books");
 
 exports.getAll = () => Books.find({});
 
+exports.getOne = (bookId) => Books.findById(bookId).lean();
+
 exports.create = (ownerId, bookData) =>
   Books.create({
     ...bookData,
     owner: ownerId,
   });
+
+exports.wish = async (userId, bookId) => {
+  const book = await Books.findById(bookId);
+  book.wishlist.push(userId);
+  return book.save();
+};
